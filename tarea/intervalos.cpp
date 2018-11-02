7137c3def49ee6af9a82fd4a096ca80053a06cbd
Santiago Martinez <martinez_santi98@hotmail.com>
2018-11-02 11:59:17 -0300

    Eliminacion de los test del codigo, agregados comentarios, todo ok

--- tarea/intervalos.cpp
+++ tarea/intervalos.cpp
@@ -11,23 +11,11 @@ struct intervalo_indexado
 };
 
 //Definiciones
-bool compatibles(intervalo_t i, intervalo_t j);
 void merge_intervalos(intervalo_indexado *intervalos, uint left, intervalo_indexado *arreglo_izq, uint izq_length, uint middle, uint right, intervalo_indexado *arreglo_der, uint der_length);
 void merge_sort_intervalos(intervalo_indexado *intervalos, uint left ,uint right);
-void imprimir(intervalo_indexado *arreglo_intervalos, uint n);
 uint max(uint a, uint b);
 
 //Implementaciones
-
-/*Imprime los intervalos del arreglo_intervalos con el
-formato {inicio_fin,inicio_fin,etc}*/
-void imprimir(intervalo_indexado *arreglo_intervalos, uint n)
-{
-  printf("{");
-  for (uint i = 0; i < n; i++)
-    printf("%d_%d,", arreglo_intervalos[i].intervalo.inicio, arreglo_intervalos[i].intervalo.fin);
-  printf("}\n");
-}
 
 /*realiza el merge entre los arreglos: arreglo_izq y arreglo_der
   en el arreglo intervalos*/
@@ -79,6 +67,8 @@ void merge_sort_intervalos(intervalo_indexado *intervalos, uint left ,uint right
   if(left != right)
   {
     uint middle = floor((left + right)/2);
+
+    //Merge_sort de los arreglos intervalos[left..middle] e intervalos[middle+1..right]
     merge_sort_intervalos(intervalos, left, middle);
     merge_sort_intervalos(intervalos, middle + 1, right);
 
@@ -130,19 +120,13 @@ bool *max_cantidad(const intervalo_t *intervalos, uint n)
   //paso_1 ordenar los intervalos por tiempo de finalizacion
   merge_sort_intervalos(arreglo_intervalos, 0, n-1);
 
-  //verificar que los intervalos esten ordenados
-  //imprimir(arreglo_intervalos, n);
-
   //paso_2 calcular la solucion final
-  //se inicializa el arreglo de la solucion
-  bool *intervalos_solucion = new bool[n];
+  bool *intervalos_solucion = new bool[n]; //se inicializa el arreglo de la solucion
   for (uint i = 0; i < n; i++)
     intervalos_solucion[i] = false;
+  intervalos_solucion[arreglo_intervalos[0].index] = true; //se añade el primer intervalo que termine antes
+  int ultimo_agregado = 0; //se marca que el ultimo intervalo agregado fue el primero (indice: 0)
 
-  //se añade el primer intervalo que termine antes
-  intervalos_solucion[arreglo_intervalos[0].index] = true;
-  //se marca que el ultimo intervalo agregado fue el primero (indice: 0)
-  int ultimo_agregado = 0;
   //se agregan el resto de intervalos a la solucion
   for (uint i = 1; i < n; i++)
   {
@@ -153,21 +137,13 @@ bool *max_cantidad(const intervalo_t *intervalos, uint n)
     }
   }
 
-  //Ver la solucion retornada
-  // printf("{");
-  // for (uint i = 0; i < n; i++) {
-  //   if (intervalos_solucion[i]) {
-  //     printf("%d_%d,",intervalos[i].inicio, intervalos[i].fin);
-  //   }
-  // }
-  // printf("}\n");
-
   delete[] arreglo_intervalos;
   return intervalos_solucion;
 }
 
 bool *max_volumen(const intervalo_t *intervalos, uint n)
 {
+    //cargamos un arreglo de intervalos con los que vienen de parametro
     intervalo_indexado *arreglo_intervalos = new intervalo_indexado[n];
     for (uint i = 0; i < n; i++)
     {
@@ -178,16 +154,13 @@ bool *max_volumen(const intervalo_t *intervalos, uint n)
     //paso_1 ordenar los intervalos por tiempo de finalizacion
     merge_sort_intervalos(arreglo_intervalos, 0, n-1);
 
-    //verificar que los intervalos esten ordenados
-    //imprimir(arreglo_intervalos, n);
-
     //paso_2 calculo de compatible_anterior(i) para todo i intervalo
     int *compatible_anterior = new int[n];
     for(uint i = 0; i < n; i++)
-    {
       compatible_anterior[i] = -1;
-    }
-    compatible_anterior[0] = -1;
+
+    compatible_anterior[0] = -1;  //el primer intervalo no tiene un compatible anterior
+    //Busqueda binaria para asignar a cada intervalo su compatible_anterior
     for(uint i = 1; i < n; i++)
     {
       int left = 0;
@@ -195,7 +168,6 @@ bool *max_volumen(const intervalo_t *intervalos, uint n)
       while(left <= right)
       {
         uint medio = floor((left + right)/2);
-        //printf("INTERVALO: %d, LEFT: %d, RIGHT: %d, MEDIO: %d\n",i,left,right, medio);
         if((medio != i) && (arreglo_intervalos[medio].intervalo.fin <= arreglo_intervalos[i].intervalo.inicio))
         {
           compatible_anterior[i] = medio;
@@ -208,12 +180,6 @@ bool *max_volumen(const intervalo_t *intervalos, uint n)
       }
     }
 
-    //Verificacion compatible anterior correcto
-    // for(uint i = 0; i < n; i++)
-    // {
-    //   printf("El P(%d) es: %d\n", i, compatible_anterior[i]);
-    // }
-
     //paso_3 Calculo del optimal
     uint *optimal = new uint[n];
     for (uint i = 0; i < n; i++)
@@ -224,14 +190,7 @@ bool *max_volumen(const intervalo_t *intervalos, uint n)
         optimal[i] = max(arreglo_intervalos[i].intervalo.volumen + optimal[compatible_anterior[i]], optimal[i - 1]);
       else
         optimal[i] = max(arreglo_intervalos[i].intervalo.volumen, optimal[i - 1]);
-      //printf("OPT[%d] = %d; VOLUMEN: %d, OPT_ANT[%d] = %d; OPT[%d] = %d\n",i, optimal[i], arreglo_intervalos[i].intervalo.volumen, compatible_anterior[i], optimal[compatible_anterior[i]], i-1, optimal[i-1]);
     }
-
-    //Verificacion del optimal
-    // for(uint i = 0; i < n; i++)
-    // {
-    //   printf("OPT[%d] = %d, VOLUMEN: %d\n", i, optimal[i], arreglo_intervalos[i].intervalo.volumen);
-    // }
 
     //paso_4 Construccion de la solucion
     bool *solucion = new bool[n];
@@ -257,15 +216,6 @@ bool *max_volumen(const intervalo_t *intervalos, uint n)
       if(j == 0)
         solucion[arreglo_intervalos[0].index] = true;
     }
-
-    // Ver la solucion retornada
-    // printf("{");
-    // for (uint i = 0; i < n; i++) {
-    //   if (solucion[i]) {
-    //     printf("%d_%d,",intervalos[i].inicio, intervalos[i].fin);
-    //   }
-    // }
-    // printf("}\n");
 
     //eliminacion de los arreglos auxiliares
     delete[] arreglo_intervalos;
